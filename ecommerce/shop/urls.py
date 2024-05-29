79ebbd048d52b27346b1d8ed8c6ba915e1dd186a
anel babu <anelbabu008@gmail.com>
2024-05-29 23:18:33 +0530

    search

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:c_slug>/', views.home, name='prod_cat'),
-    path('<slug:c_slug>/<slug:product_slug>/', views.product_item, name='details')
+    path('<slug:c_slug>/<slug:product_slug>/', views.product_item, name='details'),
+    path('search',views.search, name = 'search')
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404,Http404
 from .models import Categ,Products
+from django.db.models import Q
 
 # Create your views here.
 def home(request,c_slug = None):
@@ -21,3 +22,10 @@ def product_item(request,c_slug,product_slug):
         raise e
     return render (request,'items.html',{'pr':prod})
 
+def search(request):
+    prodt = None
+    query = None
+    if 'q' in request.GET :
+        query = request.GET.get('q')
+        prodt = Products.objects.all().filter(Q(name__contains= query)|Q(desc__contains = query)) 
+    return render(request,'search.html',{'product':prodt, 'qr':query})
