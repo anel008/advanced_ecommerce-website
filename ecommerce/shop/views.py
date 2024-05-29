320c907117bafd6c27271907243541f80a2313e0
anel babu <anelbabu008@gmail.com>
2024-05-30 01:40:59 +0530

    paginater

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404,Http404
 from .models import Categ,Products
 from django.db.models import Q
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 
 # Create your views here.
 def home(request,c_slug = None):
@@ -9,10 +10,21 @@ def home(request,c_slug = None):
     if c_slug != None :
         c_page =get_object_or_404(Categ,slug = c_slug)
         prodt = Products.objects.filter(category = c_page,available = True)
+        paginator = Paginator(prodt,4)
     else :
         prodt = Products.objects.all().filter(available = True)
+        paginator = Paginator(prodt,4)
+    try:
+            page = int(request.GET.get('page','1'))
+    except :
+            page = 1
+    try:
+            products = paginator.page(page)
+    except (EmptyPage,InvalidPage) :
+            products = paginator.page(paginator.num_pages)
+
     cat = Categ.objects.all()
-    return render(request,'index.html',{'product' : prodt, 'ct':cat})
+    return render(request,'index.html',{ 'ct':cat,'product' : products})
 
 
 def product_item(request,c_slug,product_slug):
